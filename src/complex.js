f3403f1e598f6e28642970aae8c1d52b370bc2ed
p-flock <pflock@bu.edu>
2020-06-02 00:55:15 -0400

    First commit: adding existing code

---
+++ src/complex.js
@@ -0,0 +1,29 @@
+function Complex(real, imaginary) {
+
+  this.real = real;
+  this.imag = imaginary;
+}
+
+Complex.prototype = {
+  /*
+   *getValue: function() {
+   *  return this.real + this.imaginary*Math.sqrt(2);
+   *},
+   */
+  add: function(o) {
+    return new Complex(this.real + o.real, this.imag + o.imag);
+  },
+  sub: function(o) {
+    return new Complex(this.real - o.real, this.imag - o.imag);
+  },
+  mult: function(o) {
+    var real_part = (this.real * o.real) - (this.imag * o.imag)
+    var imag_part = (this.real * o.imag) + (this.imag * o.real)
+    return new Complex(real_part, imag_part);
+
+  },
+}
+
+module.exports = {
+  Complex,
+};
---
+++ test/test.js
@@ -0,0 +1,35 @@
+var Complex = require('../src/complex.js').Complex;
+
+assert = require('assert');
+describe('Complex class implementation', function() {
+  it('new Complex()', function() {
+    var a = new Complex(1, 2);
+    var b = new Complex(-1, -2);
+    assert.equal(1, a.real);
+    assert.equal(2, a.imag);
+    assert.equal(-1, b.real);
+    assert.equal(-2, b.imag);
+  });
+  it('Addition: a.add(b)', function() {
+    var a = new Complex(1, 2);
+    var b = new Complex(3, 6);
+    var c = a.add(b)
+    assert.equal(4, c.real);
+    assert.equal(8, c.imag);
+  });
+  it('Subtraction: a.sub(b)', function() {
+    var a = new Complex(4, 7);
+    var b = new Complex(3, 6);
+    var c = a.add(b)
+    assert.equal(1, c.real);
+    assert.equal(1, c.imag);
+  });
+  it('Multiplication: a.mult(b)', function() {
+    //TODO implement Multiplication tests
+    assert.equal(true, true);
+  });
+  it('Equality a.equals(b)', function() {
+    //TODO implement equality function and write tests
+    assert.equal(true, true);
+  });
+});
